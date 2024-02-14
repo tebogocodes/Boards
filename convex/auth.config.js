b65fe1bd317638d58ab392d8b1edd6268f21fe39
tebogocodes <151438641+tebogocodes@users.noreply.github.com>
2024-02-14 09:36:47 +0200

    first commit

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+export default {
+  providers: [
+    {
+      domain: "https://huge-kid-26.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+//this is very simple but we sill need to work on th [rpeject ]
